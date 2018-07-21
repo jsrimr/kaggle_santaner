984f78f9c22c6a3adbead7bb8ccc1014d3965469
kweonwoo-jung <kweonwoo.jung@navercorp.com>
2018-07-21 13:10:38 +0900

    update prep for semi-sup

--- kaggle_statefarm_distracted_driver_detection/03_Winners_Code/tools/prepare_data_for_semi_supervised.py
+++ kaggle_statefarm_distracted_driver_detection/03_Winners_Code/tools/prepare_data_for_semi_supervised.py
@@ -3,8 +3,8 @@ import numpy as np
 import os
 import sys
 
-test_pred = sys.argv[1]
-test_pred = pd.read_csv(test_pred)
+test_pred_fname = sys.argv[1]
+test_pred = pd.read_csv(test_pred_fname)
 test_pred_probs = test_pred.iloc[:, :-1]
 test_pred_probs_max = np.max(test_pred_probs.values, axis=1)
 
@@ -17,12 +17,15 @@ print('=' * 50)
 threshold = 0.7
 count = {}
 print('# Extracting data with threshold : {}'.format(threshold))
+os.mkdir('semi_train_{}'.format(os.path.basename(test_pred_fname)))
+cmd = 'cp -r input/train input/semi_train_{}'.format(os.path.basename(test_pred_fname))
+os.system(cmd)
 for i, row in test_pred.iterrows():
   img = row['img']
   row = row.iloc[:-1]
   if np.max(row) > threshold:
     label = row.values.argmax()
-    cmd = 'cp input/test/imgs/{} input/semi_train/c{}/{}'.format(img, label, img)
+    cmd = 'cp input/test/imgs/{} input/semi_train_{}/c{}/{}'.format(img, os.path.basename(test_pred_fname), label, img)
     os.system(cmd)
     count[label] = count.get(label, 0) + 1
 

10910023d267ed7d930e01dc165cf753f23c0fce
kweonwoo-jung <kweonwoo.jung@navercorp.com>
2018-07-31 17:56:51 +0900

    update

--- kaggle_statefarm_distracted_driver_detection/03_Winners_Code/tools/ensemble.py
+++ kaggle_statefarm_distracted_driver_detection/03_Winners_Code/tools/ensemble.py
@@ -2,22 +2,16 @@ from glob import glob
 import pandas as pd
 import os
 
-resnet_weight = 0.16
-vgg19_weight = 0.16
-vgg16_weight = 0.16
+resnet_weight = 0.33
+vgg19_weight = 0.33
+vgg16_weight = 0.33
 resnet50_test = pd.read_csv('rsc/ensemble/resnet50.csv', index_col=-1).values
-resnet50_test1 = pd.read_csv('rsc/ensemble/resnet50.v2.csv', index_col=-1).values
 vgg19_test = pd.read_csv('rsc/ensemble/vgg19.csv', index_col=-1).values
-vgg19_test1 = pd.read_csv('rsc/ensemble/vgg19.v2.csv', index_col=-1).values
 vgg16_test = pd.read_csv('rsc/ensemble/vgg16.csv', index_col=-1).values
-vgg16_test1 = pd.read_csv('rsc/ensemble/vgg16.v2.csv', index_col=-1).values
 
 ensemble = resnet50_test * resnet_weight + \
-        resnet50_test1 * resnet_weight + \
         vgg19_test * vgg19_weight + \
-        vgg19_test1 * vgg19_weight + \
-        vgg16_test * vgg16_weight + \
-        vgg16_test1 * vgg16_weight
+        vgg16_test * vgg16_weight
 
 ensemble = pd.DataFrame(ensemble, columns=['c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9'])
 test_id = [os.path.basename(fl) for fl in glob('input/test/imgs/*.jpg')]
